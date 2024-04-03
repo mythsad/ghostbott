a790695ca2e0274b8088e74049aadb6afb6761ff
mythsad <165959016+mythsad@users.noreply.github.com>
2024-04-04 01:12:33 +0400

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Playing UAE Server",".gg/e-u"];
 
 
 let currentIndex = 0;
